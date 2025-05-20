fd27d2c9b6824a31ba73d8e28266c3477994b4ca
Guangxin Zhang <guangxin.zhang.bio@gmail.com>
2025-05-20 19:25:22 +0800

    testing

--- vitual test/test_copilot.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from opentrons.simulate import simulate
-runlog, bundled = simulate(Path("sci-lucif-assay4.py").open()) 
-#engine = bundled.engine  # >=v8.0 里可直接拿到
-#print(engine.state_view.liquids.get_all())   # 打印所有井的液体状态
-
-
-print(bundled)
